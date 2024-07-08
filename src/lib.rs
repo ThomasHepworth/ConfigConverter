8231d67db8ac4e023d49c52ae5f830ac47a18ccd
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2024-07-08 18:56:19 +0100

    fix(tests): include missing arguments to get the tests running

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod cli;
 mod decode;
 mod encode;
 
-pub use decode::{DecodeError, Decoder};
+pub use decode::{file_path_to_decoder, DecodeError, Decoder, FileReadError};
 pub use encode::{EncodeError, Encoder};
