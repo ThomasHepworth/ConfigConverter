2f81bfd9fc7ecdccbb909efba2d62de3a70503c9
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2024-07-08 18:57:36 +0100

    refactor: remove commented out code in main.rs
    - Tweak main so that we only have `run_cli` and imports

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,11 @@ mod decode;
 mod encode;
 
 use cli::run_cli;
-use decode::{file_path_to_decoder, Decoder};
-use encode::Encoder;
-use serde_json;
-use toml;
 
 pub use decode::DecodeError;
 pub use encode::EncodeError;
 
 fn main() {
-    // let path_to_decode = file_path_to_decoder("test.txt");
-    // match path_to_decode {
-    //     Ok(decoder) => {
-    //         println!("Decoded");
-    //     }
-    //     Err(e) => {
-    //         eprintln!("Error: {}", e);
-    //     }
-    // }
-
     match run_cli() {
         Ok(message) => println!("{}", message),
         Err(e) => eprintln!("Error: {}", e),
